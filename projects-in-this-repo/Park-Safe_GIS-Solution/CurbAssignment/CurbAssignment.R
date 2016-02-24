569cdf8eb27d2162aaa41e8a2e9c0f684f1743ee
mpancia <msp@themsp.org>
2016-02-24 07:16:38 -0800

    Fixed some line errors.

--- projects-in-this-repo/Park-Safe_GIS-Solution/CurbAssignment/CurbAssignment.R
+++ projects-in-this-repo/Park-Safe_GIS-Solution/CurbAssignment/CurbAssignment.R
@@ -1,4 +1,5 @@
 library(rgeos)
+library(zoo)
 library(dplyr)
 library(sp)
 library(geosphere)
@@ -37,25 +38,62 @@ findCNNS <- function(row){
   dists2 <- gDistance(samps, streets, byid = TRUE)
   CNNs <-apply(t(dists2), 1,getMin)
   samps$CNN <- CNNs
+  samps$ord <- 1:nrow(samps)
   samps
 }
 
 makeLine <- function(grp){
+  if(length(grp) == 1 ){
+    return(data.frame())
+  }
+  grp <- grp[order(grp$ord),]
   x <- grp$coords.x1
   y <- grp$coords.x2
-  SpatialLinesDataFrame(SpatialLines(list(Lines(Line(cbind(x,y)), ID = grp$CNN[[1]]))), data = data.frame(row.names = grp$CNN[[1]], CNN = grp$CNN[[1]]))
+  breaks <- c(1, which(zoo(grp$ord) - lag(zoo(grp$ord)) > 1), nrow(grp))
+  lineDF <- SpatialLinesDataFrame(SpatialLines(list(Lines(Line(cbind(x,y)), ID = grp$CNN[[1]]))), data = data.frame(row.names = grp$CNN[[1]], CNN = grp$CNN[[1]]))
+  coord <- as.data.frame(coordinates(lineDF))
+  ll <- vector("list", length(breaks)-1)
+  for (i in 1: (length(breaks)-1)){
+    if(breaks[i+1] == nrow(grp)){
+      subcoord <- coord[(breaks[i]):nrow(grp),]
+    } else {
+      subcoord <- coord[(breaks[i]):(breaks[i+1]-1),]
+    }
+    # check if subset contains more than 2 coordinates
+    if (nrow(subcoord) >= 2){
+      Slo1<-Line(subcoord)
+      Sli1<-Lines(list(Slo1),ID=paste0('section',i))
+      ll[[i]] <- Sli1
+    }
+    
+  }
+  nulls <- which(unlist(lapply(ll,is.null)))
+  if(length(nulls) > 0){
+    ll <- ll[-nulls]
+  }
+  lin <- SpatialLines(ll)
+  lin
 }
 
 getDF <- function(row){
   s <- findCNNS(row)
-  linelist <- s %>% data.frame %>% group_by(CNN) %>% do(l = makeLine(.)) %>% .$l 
+  linelist <- s %>% data.frame %>% group_by(CNN) %>% mutate(tot= n()) %>% filter(tot > 1) %>% do(l = makeLine(.)) %>% .$l 
   linelist
 }
 
-t <- foreach(i = 1:100, .packages = c("sp", "rgeos", "geosphere", "rgdal", "dplyr") ) %dopar% {
+t <- foreach(i = 1:nrow(feats), .packages = c("sp", "rgeos", "geosphere", "rgdal", "dplyr", "zoo") ) %dopar% {
   list(getDF(feats[i,]))
 }
 
 t <- unlist(t)
-t <- sapply(1:length(t), function(x) spChFIDs(t[[x]], as.character(x)))
+t <- sapply(1:length(t), function(x) spChFIDs(t[[x]], paste0(as.character(x),"_", 1:length(t[[x]]))))
 df <- do.call(rbind, t)
+data <- data.frame(id = sapply(df@lines, function(x) x@ID))
+rownames(data) <- sapply(df@lines, function(x) x@ID)
+df2 <- SpatialLinesDataFrame(df, data = data)
+proj4string(df2) <- proj4string(feats)
+p4s <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+transdf<- spTransform(df2, CRS= p4s)
+writeOGR(transdf, 'curbs.kml','curbs', driver='KML')
+
+
